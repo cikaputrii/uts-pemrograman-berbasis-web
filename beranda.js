8147321a0b3bd8f77028b14c1508c995efb62e4d
cikaputrii <pratamachika7@gmail.com>
2025-03-29 13:36:09 +0700

    Tambah File

---
+++ beranda.js
@@ -0,0 +1,19 @@
+function greet() {
+    alert("Halo, teman-teman!");
+}
+
+greet();
+
+let activity = "UTS Pemrograman Berbasis Web";
+console.log(activity);
+
+let dataDiri = [
+    "Nama : Chika Putri Pratama",
+    "NPM : 2310631250010",
+    "Kelas : 4B Sistem Informasi",
+    "Fakultas Ilmu Komputer"
+];
+
+console.log(dataDiri[0]);
+console.log(dataDiri[1]);
+console.log(dataDiri[2]);
